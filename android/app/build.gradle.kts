ee7df1b1853988498a8f3b18734633f35a6913bf
ibroxim <umaraliyevibrohim04@gmail.com>
2025-11-04 23:59:18 +0500

    version_code changed  4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "uz.ibroxim_ku.hisobla"
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
     }
 
     compileOptions {
